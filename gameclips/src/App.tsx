4dfe62359a036ceafea73d43357bc746dbfcf333
Nathan Boyden <nrb121903@gmail.com>
2025-07-04 16:30:17 -0400

    set up

---
+++ gameclips/src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className="flex justify-center items-center text-lg font-bold p-10">
+        Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
