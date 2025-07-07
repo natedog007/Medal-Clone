31dc239e766590111c785ad8aceb73a7f0a870cf
Nathan Boyden <nrb121903@gmail.com>
2025-07-07 01:23:18 -0400

    some set up

--- gameclips/src/App.tsx
+++ gameclips/src/App.tsx
@@ -1,9 +1,11 @@
+import NavBar from "./components/NavBar"
 
 function App() {
 
 
   return (
     <>
+      <NavBar/>
       <div className="flex justify-center items-center text-lg font-bold p-10">
         Hello World
       </div>
---
+++ gameclips/src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+function NavBar(){
+    return ( 
+        <>
+        <div className="w-screen h-15 bg-amber-50 p-5">
+            <div>
+                GameClipz
+            </div>
+        </div>
+        </>
+     );
+}
+ 
+export default NavBar;
